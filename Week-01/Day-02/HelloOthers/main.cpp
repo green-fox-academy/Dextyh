6127f0b5236259ec2840d0c88e106747ff8c501d
Dextyh <tmark.boy001@gmail.com>
2018-10-26 13:04:45 +0200

    Made the HelloOthers exercise

---
+++ Week-01/Day-02/HelloOthers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    std::cout << "Hello, Gábor!" << std::endl;
+    std::cout << "Hello, Andi!" << std::endl;
+    std::cout << "Hello, Zoli!" << std::endl;
+
+    return 0;
+}
