472ffee1bed48cc6be4eedd2c72fd3cd200a5f2a
Dextyh <tmark.boy001@gmail.com>
2018-10-26 14:46:56 +0200

    DefineBasicInfo

---
+++ Week-01/Day-02/DefineBasicInfo/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    std::string name = "Mark Toth";
+    int age = 20;
+    double height = 1.75;
+
+    std::cout << "My name is: " << name << " and I'm " << age << ". Otherwise I'm " << height << " tall." << std::endl;
+
+   bool married = false;
+
+   if (married == false) {std::cout << "No I'm not married!" << std::endl;};
+
+    return 0;
+}
