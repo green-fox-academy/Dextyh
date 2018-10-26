7e227b484b1f6dc763882c793bb2f11f5f6c9bf3
Dextyh <tmark.boy001@gmail.com>
2018-10-26 13:36:21 +0200

    Made the TwoNumbers exercise

---
+++ Week-01/Day-02/TwoNumbers/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    float a = 22;
+    float b = 13;
+    std::cout << a + b << std::endl;
+    std::cout << a - b << std::endl;
+    std::cout << a * b << std::endl;
+    std::cout << a / b << std::endl;
+
+    int aa = 22;
+    int bb = 13;
+    std::cout << aa / bb << std::endl;
+    std::cout << aa % bb << std::endl;
+
+
+
+    return 0;
+}
