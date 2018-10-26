707b955d0d5f6be5cf7a7315ed0db2f1ca8b895c
Dextyh <tmark.boy001@gmail.com>
2018-10-26 13:26:30 +0200

    Made the IntroduceYourself exercise

--- Week-01/Day-02/IntroduceYourself/main.cpp
+++ Week-01/Day-02/IntroduceYourself/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+
+    /*
+    std::cout << "Mark Toth" << std::endl;
+    std::cout << "20" << std::endl;
+    std::cout << "1.75" << std::endl;
+    */
+
+
+    std::string name = "Mark Toth";
+    int age = 20;
+    float height = 1.75;
+
+
+
+    std::cout << "My name is: " << name << std::endl;
+    std::cout << "My age is " << age << std::endl;
+    std::cout << "M height is " << height << std::endl;
+
+
+
+
     return 0;
 }
