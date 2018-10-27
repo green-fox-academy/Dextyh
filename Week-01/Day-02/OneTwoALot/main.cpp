73804ec238cded88562e0d333eea038f3acbc96f
Dextyh <tmark.boy001@gmail.com>
2018-10-27 22:32:06 +0200

    Finished the OneTwoALot exercise

---
+++ Week-01/Day-02/OneTwoALot/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main() {
+
+    int a = 0;
+    int b = 1;
+    int c = 2;
+    int d = 12;
+
+    if (a == 0) {
+        std::cout << "Not enough" << std::endl;
+    } else if (a == 1) {
+        std::cout << "One" << std::endl;
+    } else if (a==2) {
+        std::cout << "Two" << std::endl;
+    } else {
+        std::cout << "A lot" << std::endl;
+    }
+
+    if (b == 0) {
+        std::cout << "Not enough" << std::endl;
+    } else if (b == 1) {
+        std::cout << "One" << std::endl;
+    } else if (b==2) {
+        std::cout << "Two" << std::endl;
+    } else {
+        std::cout << "A lot" << std::endl;
+    }
+
+    if (c == 0) {
+        std::cout << "Not enough" << std::endl;
+    } else if (c == 1) {
+        std::cout << "One" << std::endl;
+    } else if (c==2) {
+        std::cout << "Two" << std::endl;
+    } else {
+        std::cout << "A lot" << std::endl;
+    }
+
+    if (d == 0) {
+        std::cout << "Not enough" << std::endl;
+    } else if (d == 1) {
+        std::cout << "One" << std::endl;
+    } else if (d==2) {
+        std::cout << "Two" << std::endl;
+    } else {
+        std::cout << "A lot" << std::endl;
+    }
+
+    return 0;
+}
