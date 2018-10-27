534404c747fd2c500361b579f876dac4cfe10768
Dextyh <tmark.boy001@gmail.com>
2018-10-27 13:28:39 +0200

    Finished the HelloUser exercise

---
+++ Week-01/Day-02/HelloUser/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Please enter your name here:" << std::endl;
+    std::string Name;
+    std::cin >> Name;
+    std::cout << "Hello " << Name << "!" << std::endl;
+
+    return 0;
+}
