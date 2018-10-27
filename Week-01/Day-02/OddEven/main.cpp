d6e189ee20ec07161fc452e574a5b49e2e6df582
Dextyh <tmark.boy001@gmail.com>
2018-10-27 22:24:13 +0200

    Finished OddEven exercise

---
+++ Week-01/Day-02/OddEven/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+
+    int a = 12;
+    int b = 23;
+
+    if (a % 2 == 0 ) {
+        std::cout << "Even" << std::endl;
+    } else {
+        std::cout << "Odd" << std::endl;
+
+    }
+
+    if (b % 2 == 0 ) {
+        std::cout << "Even" << std::endl;
+    } else {
+        std::cout << "Odd" << std::endl;
+
+    }
+
+
+
+    return 0;
+}
