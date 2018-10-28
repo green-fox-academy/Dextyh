3882b71dcd6f8f422000488216ba57baa4b769e6
Dextyh <tmark.boy001@gmail.com>
2018-10-28 16:01:32 +0100

    Finished the MultiplicationTable exercise

---
+++ Week-01/Day-02/MultiplicationTable/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+
+
+    int n, i;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+
+    for(i=1; i<=10; ++i)
+    {
+        printf("%d * %d = %d \n", i, n, n*i);
+    }
+
+    return 0;
+
+
+
+    return 0;
+}
---
+++ Week-01/Day-02/PrintEven/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    for (int a = 0; a <= 500; a++) {
+        if (a%2 == 0) {
+            std::cout << a << std::endl;
+        }
+
+    }
+
+
+    return 0;
+}
