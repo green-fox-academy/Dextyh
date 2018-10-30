b55ba4f66e72be5c07d78a92f1d78d4284b1b8d8
Dextyh <tmark.boy001@gmail.com>
2018-10-30 12:59:13 +0100

    Finished SimpleReplace exercise

---
+++ Week-02/Day-01/Reverse/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+
+
+int main() {
+
+    std::map<std::string, std::string>map
+
+    std::string word
+
+    map ["alma"] = "apple";
+
+
+    return 0;
+}
