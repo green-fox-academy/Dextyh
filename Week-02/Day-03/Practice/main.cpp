0fb91da131b3f3d2caf107d380e132895f44bf45
Dextyh <tmark.boy001@gmail.com>
2018-10-31 11:06:14 +0100

    Practice

---
+++ Week-02/Day-03/Practice/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+
+
+int main() {
+
+    //std::cout << "Hello, Esther!" << std::endl << "Hello, Mary!" << std::endl << "Hello, Joe!" << std::endl;
+
+
+
+
+
+    /* float weeks = 17;
+    float days = 5;
+    float hours = 6;
+
+    std::cout << "Working hours in a semester: " << weeks*days*hours << std::endl;
+
+    float whw = 52;
+
+    std:: cout << "Percentage of the coding hours in a semester: " << ((weeks*days*hours) / (whw*weeks)) * 100 << "%" << std::endl;
+    */
+
+
+
+
+    /* int cuboidA = 5;
+    int cuboidB = 7;
+    int cuboidC = 4;
+
+    int surface_area = cuboidA*cuboidB*cuboidC;
+    int volume = 2*(cuboidA*cuboidB + cuboidA*cuboidC + cuboidB*cuboidC);
+
+    std::cout << "Surface area = " << surface_area << std::endl << "Volume: " << volume << std::endl;
+    */
+
+
+
+    
+
+    /* std::cout << "Hey! Give me 5 numbers please!" << std::endl;
+
+    int something [5];
+
+    for (int i = 0; i < (sizeof(something) / sizeof(something[0])); ++i) {
+
+        std::cin >> something[i] ;
+
+        }
+
+        std::cout << "Do you wanna know their SUM and Average? " << std::endl << "If yes type: 1 // If no type: 0" << std::endl;
+
+
+        int SUM = 0;
+
+        for (int i = 0; i < (sizeof(something) / sizeof(something[0])); ++i) {
+            SUM += something[i];
+        }
+
+        int average = SUM / (sizeof(something) / sizeof(something[0]));
+
+
+        bool yesno;
+        std::cin >> yesno;
+
+        if (yesno) {
+            std::cout << "SUM: " << SUM << std::endl << "Average: " << average << std::endl;
+
+        } else {
+            std::cout << "Okay, it's on you, BB! " << std::endl;
+        }
+    */
+
+
+
+
+    return 0;
+}
+
+
