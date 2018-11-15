a7c85721965e38cace48a4a0ed60befa0a0d499d
Dextyh <tmark.boy001@gmail.com>
2018-11-15 11:45:06 +0100

    Finished SumDigit exercise

--- Week-04/Day-04/SumDigit/main.cpp
+++ Week-04/Day-04/SumDigit/main.cpp
@@ -1,10 +1,33 @@
+// Given a non-negative int n, return the sum of its digits recursively (no loops).
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+
 #include <iostream>
 
-// Given a non-negative int n, return the sum of its digits recursively (no loops). 
-// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
-// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+int digitsSum(int);
 
 int main() {
 
+    int number;
+
+    std::cout << "Type a non-negative number here: ";
+    std::cin >> number;
+
+    std::cout << digitsSum(number) << std::endl;
+
+
     return 0;
+}
+
+
+int digitsSum (int n) {
+
+    if (n <= 0) {
+        return n;
+
+    } else {
+        return (n % 10) + digitsSum(n/10);
+    }
+
 }
