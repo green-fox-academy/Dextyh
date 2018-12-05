24550d0ace7d1a92abe601b2541f493acb4a838e
Dextyh <tmark.boy001@gmail.com>
2018-12-05 16:50:52 +0100

    Finished IndexOOfIt exercise

---
+++ Week-07/Day-01/IndexOfIt/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int isIn(char* sentence, char* word);
+
+int main() {
+
+    char *sentence = "An apple a day keeps the doctor away.";
+    char *word = "doctor";
+
+    printf("%d", isIn(sentence, word));
+
+    return 0;
+}
+
+int isIn(char * sentence, char * word)
+{
+    int wordCounter = 0;
+
+    for (int i = 0; sentence[i] != '\0'; ++i) {
+        if(sentence[i] == ' ') {
+            wordCounter++;
+        } else if(sentence[i] == word[0]) {
+            int bool = 1;
+            int newI = i;
+
+            for (int j = 0; j < strlen(word); ++j) {
+                if(sentence[newI] != word[j]) {
+                    bool = 0;
+                    break;
+                }
+                newI++;
+            }
+            if (bool == 1) {
+                printf("Your world is in the sentence!");
+                return wordCounter;
+            }
+        }
+    }
+    printf("Your world is NOT in the sentence!");
+    return 0;
+}
