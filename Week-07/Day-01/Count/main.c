ee5c271ba9c6b77245034e070b116ad2db99fa16
Dextyh <tmark.boy001@gmail.com>
2018-12-05 16:20:09 +0100

    Finished CountBetweenChars exercise

--- Week-07/Day-01/Count/main.c
+++ Week-07/Day-01/Count/main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int count(char * string);
+int count(char * word, char character);
 
 int main() {
 
+    char * word = "Programming";
+    char character = 'g';
 
+    printf("%d", count(word, character));
 
     return 0;
 }
 
-int count(char * string) {
+int count(char * string, char character) {
 
+    int index = 0;
 
+    for (int i = 0; string[i] != character ; ++i) {
+        index++;
+    }
+    int counter = 0;
+
+    while(string[index+1] != character) {
+        index++;
+        counter++;
+    }
+    return counter;
 }
