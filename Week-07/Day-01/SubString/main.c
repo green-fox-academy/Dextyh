6423915c73cf7bf4116769b38f8638399da8774e
Dextyh <tmark.boy001@gmail.com>
2018-12-05 17:11:53 +0100

    UPGRADE on SubString

--- Week-07/Day-01/SubString/main.c
+++ Week-07/Day-01/SubString/main.c
@@ -8,14 +8,17 @@ int main() {
     char * string1 = "This is a big tit!";
     char * string2 = "big";
 
-    printf("%d", subString(string1, string2));
+    printf("%d\n", subString(string1, string2));
+    printf("%s\n", strstr(string1, string2));
 
     return 0;
 }
 
 int subString(char * string1, char * string2)
 {
-    if(strstr(string1, string2) && strlen(string1) != strlen(string2)) {
+    char* firstOccurrence = strstr(string1, string2);
+
+    if(firstOccurrence != NULL && strlen(string1) != strlen(string2)) {
         return 1;
     } else if (strlen(string1) == strlen(string2)) {
         return -1;
