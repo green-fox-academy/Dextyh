7fc952565825c558bfc4fc640572425838c539c6
Dextyh <tmark.boy001@gmail.com>
2018-12-07 13:26:16 +0100

    Finished CountLines exercise

--- Week-07/Day-03/PrintLines/main.c
+++ Week-07/Day-03/PrintLines/main.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int line_counter(char * filename);
+
 int main() {
 
     FILE * file_pointer;
-    file_pointer = fopen("my-file.txt", "w");
-
-    fprintf(file_pointer, "Valami1\n");
-    fprintf(file_pointer, "Valami2\n");
-    fprintf(file_pointer, "Valami3");
-
-
-    fclose(file_pointer);
-
-
     file_pointer = fopen("my-file.txt", "r");
 
     char single_line[100];
@@ -21,7 +13,28 @@ int main() {
         fgets(single_line, 100, file_pointer);
         puts(single_line);
     }
+
     fclose(file_pointer);
 
+    printf("%d", line_counter("my-file.txt"));
+
     return 0;
+}
+
+int line_counter(char * filename)
+{
+    FILE * file_pointer;
+    file_pointer = fopen(filename, "r");
+    if(!fopen(filename, "r")) {
+        return -1;
+    }
+
+    int l_counter = 0;
+    char single_line[100];
+    while (!feof(file_pointer)){
+        fgets(single_line, 100, file_pointer);
+        l_counter++;
+    }
+
+    return l_counter;
 }
