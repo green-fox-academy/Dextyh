0f60d8c7bfec9df64ea7ae3f146c533579f7aeb1
Dextyh <tmark.boy001@gmail.com>
2018-12-11 13:59:25 +0100

    Finished ConcatStrings

---
+++ Week-08/Day-02/ConcatStrings/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* connect(char* first_name, char* last_name);
+
+int main() {
+
+    char* first_name = "Mark ";
+    char* last_name = "Toth\n";
+
+    printf("%s",connect(first_name, last_name));
+
+    return 0;
+}
+
+char* connect(char* first_name, char* last_name)
+{
+    int size = (int)strlen(first_name) + (int)strlen(last_name);
+
+    char* full_name = calloc(size + 1, sizeof(char));
+
+    strcat(full_name,first_name);
+    strcat(full_name,last_name);
+
+    if(strlen(full_name) == 0) {
+        return "This string is empty you little bastard!\n";
+    } else {
+        return full_name;
+    }
+}
